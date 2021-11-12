e33b05eecea1c551ce6562fd4d73a831b83e38a1
Anderson <laventurepsu1@gmail.com>
2021-11-12 12:44:38 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.scss';
+
+import firebase from './FireBaseConfig'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title-row">
+      <h1 className="title">Firebase recipes</h1>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
